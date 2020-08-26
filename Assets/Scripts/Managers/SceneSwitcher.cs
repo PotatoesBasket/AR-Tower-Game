b030eed705b94f5019fcffd26c3bd9847fab2890
PotatoesBasket <PotatoesBasket@hotmail.com>
2020-08-26 17:00:42 +1000

    Some things
    settings

---
+++ Assets/Scripts/Managers/SceneSwitcher.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitcher : MonoBehaviour
+{
+    public void LoadMenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+
+    public void LoadLV1()
+    {
+        SceneManager.LoadScene("PuzzleTower1 (V2)");
+    }
+}
