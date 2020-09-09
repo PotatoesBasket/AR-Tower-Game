8cbf23c0043a151dcd6ceef52e1ce258a367cb11
PotatoesBasket <PotatoesBasket@hotmail.com>
2020-09-09 19:05:20 +1000

    Added LV2 to menu

--- Assets/Scripts/Managers/SceneSwitcher.cs
+++ Assets/Scripts/Managers/SceneSwitcher.cs
@@ -14,4 +14,9 @@ public class SceneSwitcher : MonoBehaviour
     {
         SceneManager.LoadScene("PuzzleTower1T3");
     }
+
+    public void LoadLV2()
+    {
+        SceneManager.LoadScene("PuzzleTower2T3");
+    }
 }
