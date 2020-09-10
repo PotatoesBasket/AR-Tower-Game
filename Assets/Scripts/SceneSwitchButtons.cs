2dd062059f8b25080053c0e810972e71b7e7267f
PotatoesBasket <PotatoesBasket@hotmail.com>
2020-09-10 22:32:15 +1000

    Menu button fix
    Goes to levels properly after reload now

---
+++ Assets/Scripts/SceneSwitchButtons.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneSwitchButtons : MonoBehaviour
+{
+    SceneSwitcher s;
+
+    public void GoToMenu()
+    {
+        s = GameObject.FindGameObjectWithTag("Manager").GetComponent<SceneSwitcher>();
+        s.LoadMenu();
+    }
+
+    public void GoToLv1()
+    {
+        s = GameObject.FindGameObjectWithTag("Manager").GetComponent<SceneSwitcher>();
+        s.LoadLV1();
+    }
+
+    public void GoToLv2()
+    {
+        s = GameObject.FindGameObjectWithTag("Manager").GetComponent<SceneSwitcher>();
+        s.LoadLV2();
+    }
+}
